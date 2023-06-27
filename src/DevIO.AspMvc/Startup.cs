5403053cd25e89482d89778181930031cb6cf0c3
JeffersonTales <jefferson.canalli@hotmail.com>
2023-06-27 09:16:34 -0300

    criacao projeto

---
+++ src/DevIO.AspMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevIO.AspMvc.Startup))]
+namespace DevIO.AspMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
