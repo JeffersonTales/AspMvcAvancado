a81990ec18eea54a913f1cf1bd2c4837c31091e7
JeffersonTales <jefferson.canalli@hotmail.com>
2023-06-30 17:23:36 -0300

    implementacao parcial na camada mvc

--- src/DevIO.AspMvc/Controllers/FornecedoresController.cs
+++ src/DevIO.AspMvc/Controllers/FornecedoresController.cs
@@ -11,15 +11,18 @@ using System.Web;
 using System.Web.Mvc;
 
 namespace DevIO.AspMvc.Controllers {
+
     public class FornecedoresController : BaseController {
 
         #region Atributos
         private readonly IFornecedorRepository _fornecedorRepository;
         private readonly IFornecedorService _fornecedorService;
         private readonly IMapper _mapper;
+        #endregion
 
-        public FornecedoresController(IFornecedorRepository fornecedorRepository, 
-                                      IFornecedorService fornecedorService, 
+        #region Construtor
+        public FornecedoresController(IFornecedorRepository fornecedorRepository,
+                                      IFornecedorService fornecedorService,
                                       IMapper mapper) {
 
             this._fornecedorRepository = fornecedorRepository;
@@ -30,20 +33,103 @@ namespace DevIO.AspMvc.Controllers {
 
         #region Metodos
 
-        [Route(template:"lista-de-fornecedores")]
+        [Route(template: "lista-de-fornecedores")]
         public async Task<ActionResult> Index() {
 
             return View(this._mapper.Map<IEnumerable<FornecedorViewModel>>(await this._fornecedorRepository.ObterTodos()));
 
         }
 
-        private async Task<FornecedorViewModel> ObterFornecedorEndereco(Guid id) {
+        [Route(template: "editar-fornecedor/{id:guid}")]
+        public async Task<ActionResult> Edit(Guid id) {
 
-            return this._mapper.Map<FornecedorViewModel>(await this._fornecedorRepository.ObterFornecedorEndereco(id)); 
-        
+            var fornecedorViewModel = await ObterFornecedorProdutosEndereco(id);
+
+            if (fornecedorViewModel == null) return HttpNotFound();
+
+            return View(model: fornecedorViewModel);
+
         }
 
-        //07:25
+        [Route(template:"dados-do-fornecedor/{id:guid}")]
+        public async Task<ActionResult> Details(Guid id) {
+
+            var fornecedorViewModel = await ObterFornecedorEndereco(id);
+
+            if (fornecedorViewModel == null) return HttpNotFound();
+
+            return View(model: fornecedorViewModel);
+
+        }
+
+        [Route(template: "novo-fornecedor")]
+        public ActionResult Create() {
+            return View();
+        }
+
+        [Route(template:"novo-fornecedor")]
+        [HttpPost]
+        public async Task<ActionResult> Create(FornecedorViewModel fornecedorViewModel) {
+
+            if (!this.ModelState.IsValid) return View(model: fornecedorViewModel);
+
+            var fornecedor = this._mapper.Map<Fornecedor>(fornecedorViewModel);
+
+            await this._fornecedorService.Adicionar(fornecedor);
+
+
+            //TODO: posteriormente fazer tratamento para notificar o usuário quando a inclusão não for bem sucedida.
+            return RedirectToAction("Index");   
+
+        }
+
+        [Route(template: "editar-fornecedor/{id:guid}")]
+        [HttpPost]
+        public async Task<ActionResult> Edit(Guid id,
+                                             FornecedorViewModel fornecedorViewModel) {
+
+            if (id != fornecedorViewModel.Id) return HttpNotFound();
+
+            if (!this.ModelState.IsValid) return View(model: fornecedorViewModel);
+
+            var fornecedor = this._mapper.Map<Fornecedor>(fornecedorViewModel);
+
+            await this._fornecedorService.Atualizar(fornecedor);
+
+            //TODO: posteriormente fazer tratamento para notificar o usuário quando a alteração não for bem sucedida.
+            return RedirectToAction(actionName: "Index");   
+        }
+
+        [Route(template: "excluir-fornecedor/id{id:guid}")]
+        public async Task<ActionResult> Delete(Guid id) {
+
+            var fornecedorViewModel = await ObterFornecedorEndereco(id);
+
+            if(fornecedorViewModel == null) return HttpNotFound();
+
+            //TODO: posteriormente fazer tratamento para notificar o usuário quando a exclusão não for bem sucedida.
+            return View(fornecedorViewModel);
+        }
+
+        [Route(template: "excluir-fornecedor/{id:guid}")]
+        [HttpPost, ActionName("Delete")]
+        public async Task<ActionResult> DeleteConfirmed(Guid id) {
+
+            var fornecedor = await ObterFornecedorEndereco(id);
+
+            if(fornecedor == null) return HttpNotFound();
+
+            await this._fornecedorService.Remover(id);
+
+            return RedirectToAction(actionName: "Index");
+        }
+
+        private async Task<FornecedorViewModel> ObterFornecedorEndereco(Guid id) {
+
+            return this._mapper.Map<FornecedorViewModel>(await this._fornecedorRepository.ObterFornecedorEndereco(id));
+
+        }
+
         private async Task<FornecedorViewModel> ObterFornecedorProdutosEndereco(Guid id) {
 
             return this._mapper.Map<FornecedorViewModel>(await this._fornecedorRepository.ObterFornecedorProdutosEndereco(id));
--- src/DevIO.AspMvc/ViewModels/FornecedorViewModel.cs
+++ src/DevIO.AspMvc/ViewModels/FornecedorViewModel.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
 
 namespace DevIO.AspMvc.ViewModels {
     public class FornecedorViewModel {
@@ -14,14 +12,14 @@ namespace DevIO.AspMvc.ViewModels {
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
-        public string Name { get; set; }
+        public string Nome { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
         public string Documento { get; set; }
 
         [DisplayName("Tipo")]
-        public int Tipo { get; set; }
+        public int TipoFornecedor { get; set; }
 
         public EnderecoViewModel Endereco { get; set; }
 
